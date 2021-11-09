bda5352616c57b1425d104be4b994d8f1005b23a
prigit2021 <priya21.boopathy@gmail.com>
2021-11-09 13:32:40 +0530

    Folder structure

---
+++ src/org/priya/FbLogin.java
@@ -0,0 +1,13 @@
+package org.priya;
+
+public class FbLogin {
+	public static void username() {
+		System.out.println("my name is priya");
+	}
+
+	public static void main(String[] args) {
+		username();
+
+	}
+
+}
